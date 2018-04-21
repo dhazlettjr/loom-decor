ce2a91189d2650fdafecb65572290ae3679b391f
dhazlettjr <d.hazlett.jr@gmail.com>
2018-04-21 16:07:45 -0500

    created a factory to GET products, types and categories in order to send to the formatter then view

---
+++ javascripts/factory.js
@@ -0,0 +1,46 @@
+"use strict";
+
+const $ = require("jquery");
+
+module.exports.getCategories = () => {
+  console.log("These are cats");
+  return new Promise((resolve, reject) => {
+    $.ajax({
+      url: "../data/categories.json"
+    })
+      .done(cats => {
+        resolve(cats.categories);
+      })
+      .fail(error => {
+        reject(error);
+      });
+  });
+};
+
+module.exports.getProducts = () => {
+  return new Promise((resolve, reject) => {
+    $.ajax({
+      url: "../data/products.json"
+    })
+      .done(prods => {
+        resolve(prods.products);
+      })
+      .fail(error => {
+        reject(error);
+      });
+  });
+};
+
+module.exports.getTypes = () => {
+  return new Promise((resolve, reject) => {
+    $.ajax({
+      url: "../data/types.json"
+    })
+      .done(types => {
+        resolve(types.types);
+      })
+      .fail(error => {
+        reject(error);
+      });
+  });
+};
