bd7878f10391e97dce93a098359b52d28633cd91
dhazlettjr <d.hazlett.jr@gmail.com>
2018-04-25 17:04:44 -0500

    one last fix

--- javascripts/factory.js
+++ javascripts/factory.js
@@ -6,7 +6,7 @@ const $ = require("jquery");
 module.exports.getCategories = () => {
   return new Promise((resolve, reject) => {
     $.ajax({
-      url: "http://dhazlettjr.github.io/categories.json"
+      url: "https://dhazlettjr.github.io/loom-decor/data/categories.json"
     })
       .done(cats => {
         resolve(cats.categories);
@@ -20,7 +20,7 @@ module.exports.getCategories = () => {
 module.exports.getProducts = () => {
   return new Promise((resolve, reject) => {
     $.ajax({
-      url: "http://dhazlettjr.github.io/products.json"
+      url: "https://dhazlettjr.github.io/loom-decor/data/products.json"
     })
       .done(prods => {
         resolve(prods.products);
@@ -34,7 +34,7 @@ module.exports.getProducts = () => {
 module.exports.getTypes = () => {
   return new Promise((resolve, reject) => {
     $.ajax({
-      url: "http://dhazlettjr.github.io/types.json"
+      url: "https://dhazlettjr.github.io/loom-decor/data/types.json"
     })
       .done(types => {
         resolve(types.types);
