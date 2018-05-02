7fe1082088a5c6db361df5521832fdf524f4d3fc
David A Hazlett Jr <d.hazlett.jr@gmail.com>
2018-05-02 11:03:16 -0500

    Update formatter.js

--- javascripts/formatter.js
+++ javascripts/formatter.js
@@ -12,10 +12,13 @@ module.exports.formatData = data => {
   //im going to create a new array to match the category and type ids to the product itself
 
   let revisedProds = products.map(prod => {
+    //prod is a full list of products
     let currentProd = Object.keys(prod);
+    //currentProd is each individual prod
     let prodType = types.find(type => type.id === prod[currentProd].type );
     let prodCat = cats.find( category => category.id === prodType.category);
-
+    
+  //this will give the type name to the current prod
     prod[currentProd].type = prodType.name;
     prod[currentProd].category = prodCat.name;
 
