de144b4f626e9782edfa00d3086b87322159995e
David A Hazlett Jr <d.hazlett.jr@gmail.com>
2018-05-02 10:43:02 -0500

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,19 +1,25 @@
 "use strict";
 
 const $ = require("jquery");
+//pull in data from the factory
 const factory = require("./factory");
+//pull in formatted data
 const formatter = require('./formatter');
+//pull in display products
 const prodView = require('./prodView');
 
-const loom = [];
 
 //array of factory gets put into a prmose
+//I did this so I wouldnt have to keep chaining .then
+
 let promArr = [
   factory.getCategories(),
   factory.getTypes(),
   factory.getProducts()
 ];
+
 //run a promise all to gather all GETs at the same time
+
 Promise.all(promArr)
 .then( (dataArr) => {
   let revisedProds = formatter.formatData(dataArr);
